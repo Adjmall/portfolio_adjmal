d6a50e6ece8075d3d2917e698b35ad21ec2d19c6
adjmal <adjmalahmed@hotmail.com>
2023-02-21 16:53:46 +0100

    ajout page

---
+++ acceuil.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Adjmal</title>
+    <link rel="stylesheet" href="./porfolio.css">
+</head>
+
+<body>
+    <nav></nav>
+
+
+
+
+<div id="mySidenav" class="sidenav">
+  <a id="closeBtn" href="#" class="close">×</a>
+  <ul>
+    <li><a href="#">A propos</a></li>
+    <li><a href="#">Nos services</a></li>
+    <li><a href="#">Témoignages</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+</div>
+
+<a href="#" id="openBtn">
+  <span class="burger-icon">
+    <span></span>
+    <span></span>
+    <span></span>
+  </span>
+</a>
+
+
+</body>
+
+
+<script src="./portfolio.js"></script>
+</html>
---
+++ acceuil1.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PortfolioAdjmal</title>
+    <link rel="stylesheet" href="./portfolio1.css">
+</head>
+
+<body>
+
+    <nav>
+        <div class="nav1">
+            <a href="">Acceuil</a>
+            <a href="">A propos</a>
+            <a href="">Contact</a>
+
+        </div>
+        <div class="nav2">
+            <a href=""><img src="./Le-logo-Facebook.jpg" alt="" class="logofacebook"></a>
+            <a href=""><img src="./téléchargement.jpg" alt="" class="logoinsta"></a>
+
+        </div>
+    </nav>
+
+    <h2> Adjmal</h2>
+    <aside>
+        <img src="./profil.jpg" alt="" class="imgprofil">
+        <p>
+            Ayant fait eu un bac scientifique, par rapport à mes études supérieures, j’ai tout de suite eu envi de faire des études en biologie plus précisément en agro-alimentaire dont j’ai pu intégrer en master et malheureusement je n’ai pas pu finir mon cursus à cause d’un refus administratif. Par ailleurs, en parallèle, je travaillais en tant que technicien support IT et je suis retrouvé dans un univers très large ce qui m’a poussé via des recherches personnelles à m’intégrer dans le développement web par lequel je me sens facilement à l’aise et que je me rends compte que c’est vraiment ce que j’aime et que je crois finir ma carrière professionnelle dans ce domaine.
+        </p>
+    </aside>
+    <p class="avantProjet">
+    Certains de mes derniers travaux
+    </p>
+
+    <div class="blog">
+  
+    <div class="title-box">
+      <h3>
+      SPRING FEVER
+      </h3>
+      <hr/>
+      <div class="intro">
+        Yllamco laboris nisi ut aliquip ex ea commodo.
+      </div>
+    </div>  
+    <div class="info">
+      <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</span>
+  </div>
+  <div class="footer">
+    <div class="icon-holder">
+      <span>
+    <i class="fa fa-comment-o"></i>
+      <span>12</span>
+      <space></space>
+      <i class="fa fa-calendar"></i>
+      <span>03.12.2015</span>
+      </span>
+    </div>
+  </div>
+  
+ <div class="color-overlay"></div>
+</div>
+
+</body>
+
+</html>
